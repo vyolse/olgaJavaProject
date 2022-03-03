001fef8678563b0d9606e55398c0e26a9752d0f2
vyolse <vyolse@gmail.com>
2022-03-03 23:05:59 +0300

    first commit

---
+++ myHomeWork/src/main/java/lesson1/myHomeWork.java
@@ -0,0 +1,48 @@
+package lesson1;
+
+public class myHomeWork {
+    public static void main(String[] args) {
+        printThreeWords();
+        checkSumSign();
+        printColor();
+        compareNumbers();
+    }
+    public static void printThreeWords(){
+        System.out.println("Orange");
+        System.out.println("Banana");
+        System.out.println("Apple");
+    }
+    public static void checkSumSign(){
+        int a = 3;
+        int b = 7;
+        int c = a + b;
+        if (c >= 0){
+            System.out.println("Сумма положительная");
+        }
+        else {
+            System.out.println("Сумма отрицательная");
+        }
+    }
+    public static void printColor() {
+        int value = 9;
+        if (value <= 0) {
+            System.out.println("Красный");}
+
+        else if (value >0 && value <= 100) {
+            System.out.println("Желтый");
+        }
+        else{
+            System.out.println("Зелёный");
+        }
+    }
+    public static void compareNumbers() {
+        int a = 2;
+        int b = 3;
+        if (a >= b) {
+            System.out.println("a>=b");
+        }
+        else{
+            System.out.println("a<b");
+        }
+    }
+}
